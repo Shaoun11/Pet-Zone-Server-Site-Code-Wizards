cd358888022055b73f5621dc95d985ba3682d703
afifa <2023afifa@gmail.com>
2024-01-24 16:30:54 +0600

    New code added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
- 
+
 
 
 const uri = "mongodb+srv://Pet-Zone:IVvvOV3guIrkP8Ly@cluster0.3b178xe.mongodb.net/?retryWrites=true&w=majority";
@@ -22,45 +22,65 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }, 
+  },
 });
 
-   
+
 
 
 
 
 async function run() {
   try {
-      // Connect the client to the server	(optional starting in v4.7)
-      
- const petCollection = client.db("PetZone").collection("Petdata");
- 
+    // Connect the client to the server	(optional starting in v4.7)
+
+    const petCollection = client.db("PetZone").collection("Petdata");
+    const userCollection = client.db("PetZone").collection("users");
 
 
-       app.get("/petdata", async (req, res) => {
-        const result = await petCollection.find().toArray();
-        res.send(result);
-       });
-  
+
+    app.get("/petdata", async (req, res) => {
+      const result = await petCollection.find().toArray();
+      res.send(result);
+    });
+
     app.get("/petdata/:_id", async (req, res) => {
       const id = req.params._id;
       const query = { _id: new ObjectId(id) };
       const result = await petCollection.findOne(query);
       res.send(result);
     });
-  
- 
-  
 
-   client.db("admin").command({ ping: 1 });
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users);
+    })
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists", insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
+
+    client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-      
-      
-   
-       
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
