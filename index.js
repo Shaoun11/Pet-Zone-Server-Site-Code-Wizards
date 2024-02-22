8bf0df5cb2627469d5c7af285578912109f95f8e
rezwan2230 <rezwan2230@gmail.com>
2024-02-23 01:59:36 +0600

    Delete a data by id.

--- index.js
+++ index.js
@@ -417,6 +417,16 @@ async function run() {
       res.send(result)
     })
 
+    //Delete a data by id.
+    app.delete('/mypet/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await petCollection.deleteOne(query);
+      res.send(result)
+    })
+
+
+
     
 
 
